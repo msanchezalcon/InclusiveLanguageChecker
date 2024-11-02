42e66859fab48ffc10c5d8067a83740b12c3936c
Miriam Sanchez Alcon <46067629+msanchezalcon@users.noreply.github.com>
2024-11-03 00:15:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,8 @@ const github = require('@actions/github');
 const fs = require('fs');
 const path = require('path');
 
-console.log('Starting action...');
-
-console.log('Current working directory:', process.cwd());
-
-console.log('Environment variables:', process.env);
-
 const loadTerms = () => {
   const filePath = path.join(__dirname, 'nonInclusiveWords.json');
-  console.log('Loading terms from:', filePath);
-  
   const content = fs.readFileSync(filePath, 'utf-8');
   return JSON.parse(content).terms;
 };
@@ -22,15 +14,18 @@ const checkFiles = async (files) => {
   let comments = [];
 
   for (const file of files) {
-    console.log('Checking file:', file);
-    const content = fs.readFileSync(file, 'utf-8');
-    
-    Object.keys(termsMap).forEach((term) => {
-      if (content.includes(term)) {
-        const suggestions = termsMap[term].join(', ');
-        comments.push(`Non-inclusive term found in ${file}: "${term}". Suggested replacements: ${suggestions}.`);
-      }
-    });
+    const filePath = path.join(process.cwd(), file);
+    if (fs.existsSync(filePath)) {
+      const content = fs.readFileSync(filePath, 'utf-8');
+      Object.keys(termsMap).forEach((term) => {
+        if (content.includes(term)) {
+          const suggestions = termsMap[term].join(', ');
+          comments.push(`Non-inclusive term found in ${file}: "${term}". Suggested replacements: ${suggestions}.`);
+        }
+      });
+    } else {
+      core.warning(`File not found: ${filePath}`);
+    }
   }
 
   return comments;
@@ -38,15 +33,15 @@ const checkFiles = async (files) => {
 
 async function run() {
   try {
-    const files = process.argv.slice(2);
-    console.log('Files to check:', files);
+    const filesInput = core.getInput('files')
+    const files = filesInput.split(',').map(file => file.trim());
+    
     const comments = await checkFiles(files);
     
     if (comments.length > 0) {
       const context = github.context;
       const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
-
-      const pullRequestNumber = context.payload.pull_request.number;
+      const pullRequestNumber = context.payload.pull_request?.number;
 
       if (pullRequestNumber) {
         for (const comment of comments) {
